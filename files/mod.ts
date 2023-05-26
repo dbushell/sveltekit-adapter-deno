33ac97aa0672adc0f0a0f5fb3227f7a14d18fe42
David Bushell <david@dbushell.com>
2023-05-26 16:00:28 +0100

    handle trailing slash better (#1)

--- files/mod.ts
+++ files/mod.ts
@@ -1,7 +1,9 @@
 import type {ConnInfo, Handler} from 'server';
 import * as path from 'path';
 import {serve} from 'server';
-import {serveDir} from 'file_server';
+import {serveDir, serveFile} from 'file_server';
+
+import {manifest, prerendered} from './server/manifest.js';
 
 import server from './server.js';
 
@@ -10,20 +12,42 @@ const initialized = server.init({env: Deno.env.toObject()});
 const baseDir = path.dirname(new URL(import.meta.url).pathname);
 const rootDir = path.join(baseDir, 'static');
 
+const serveDirOptions = {
+  fsRoot: rootDir,
+  quiet: true
+};
+
 const handler: Handler = async (request: Request, context: ConnInfo) => {
   // Get client IP address
   const clientAddress =
     request.headers.get('x-forwarded-for') ??
     (context.remoteAddr as Deno.NetAddr).hostname;
 
+  const {pathname} = new URL(request.url);
+
+  // Try prerendered route without extension
+  if (prerendered.has(pathname) && !path.extname(pathname)) {
+    const response = await serveFile(
+      request,
+      path.join(rootDir, `${pathname}.html`)
+    );
+    if (response.ok) {
+      return response;
+    }
+  }
+
   // Try static files
-  const response = await serveDir(request, {
-    fsRoot: rootDir,
-    quiet: true
-  });
-  if (response && (response.ok || response.status < 400)) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/_app/immutable/')) {
+  const response = await serveDir(request, serveDirOptions);
+  if (response.status === 301) {
+    // Handle trailing slash redirects
+    if (prerendered.has(`${pathname}/`)) {
+      return response;
+    }
+  } else if (response.status < 400) {
+    if (
+      pathname.startsWith(`/${manifest.appDir}/immutable/`) &&
+      response.status === 200
+    ) {
       response.headers.set(
         'cache-control',
         'public, max-age=31536000, immutable'
