7f2c2a7f89a67b1da2bf2e8fa076d0ef3479642a
David Bushell <david@dbushell.com>
2023-08-02 09:39:21 +0100

    replace deprecated serve with Deno.serve

--- files/mod.ts
+++ files/mod.ts
@@ -1,5 +1,3 @@
-import type {ConnInfo, Handler} from 'server';
-import {serve} from 'server';
 import {serveDir, serveFile} from 'file_server';
 import {dirname, extname, join} from 'path';
 
@@ -13,67 +11,62 @@ const appDir = 'APP_DIR';
 const baseDir = dirname(new URL(import.meta.url).pathname);
 const rootDir = join(baseDir, 'static');
 
-const handler: Handler = async (request: Request, context: ConnInfo) => {
-  // Get client IP address
-  const clientAddress =
-    request.headers.get('x-forwarded-for') ??
-    (context.remoteAddr as Deno.NetAddr).hostname;
+Deno.serve(
+  async (request: Request, info: Deno.ServeHandlerInfo): Promise<Response> => {
+    // Get client IP address
+    const clientAddress =
+      request.headers.get('x-forwarded-for') ?? info.remoteAddr.hostname;
 
-  const {pathname} = new URL(request.url);
+    const {pathname} = new URL(request.url);
 
-  // Path has trailing slash
-  const slashed = pathname.at(-1) === '/';
+    // Path has trailing slash
+    const slashed = pathname.at(-1) === '/';
 
-  // Handle trailing slash redirects for prerendered routes
-  const location = slashed ? pathname.slice(0, -1) : `${pathname}/`;
-  if (prerendered.has(location)) {
-    return new Response(null, {
-      status: 308,
-      statusText: 'Permanent Redirect',
-      headers: {
-        location
+    // Handle trailing slash redirects for prerendered routes
+    const location = slashed ? pathname.slice(0, -1) : `${pathname}/`;
+    if (prerendered.has(location)) {
+      return new Response(null, {
+        status: 308,
+        statusText: 'Permanent Redirect',
+        headers: {
+          location
+        }
+      });
+    }
+
+    // Try prerendered route with html extension
+    if (!slashed && !extname(pathname) && prerendered.has(pathname)) {
+      const response = await serveFile(
+        request,
+        join(rootDir, `${pathname}.html`)
+      );
+      if (response.ok || response.status === 304) {
+        return response;
       }
-    });
-  }
+    }
 
-  // Try prerendered route with html extension
-  if (!slashed && !extname(pathname) && prerendered.has(pathname)) {
-    const response = await serveFile(
-      request,
-      join(rootDir, `${pathname}.html`)
-    );
+    // Try static files (ignore redirects and errors)
+    const response = await serveDir(request, {
+      fsRoot: rootDir,
+      quiet: true
+    });
     if (response.ok || response.status === 304) {
+      if (
+        pathname.startsWith(`/${appDir}/immutable/`) &&
+        response.status === 200
+      ) {
+        response.headers.set(
+          'cache-control',
+          'public, max-age=31536000, immutable'
+        );
+      }
       return response;
     }
+
+    // Pass to the SvelteKit server
+    await initialized;
+    return server.respond(request, {
+      getClientAddress: () => clientAddress
+    });
   }
-
-  // Try static files (ignore redirects and errors)
-  const response = await serveDir(request, {
-    fsRoot: rootDir,
-    quiet: true
-  });
-  if (response.ok || response.status === 304) {
-    if (
-      pathname.startsWith(`/${appDir}/immutable/`) &&
-      response.status === 200
-    ) {
-      response.headers.set(
-        'cache-control',
-        'public, max-age=31536000, immutable'
-      );
-    }
-    return response;
-  }
-
-  // Pass to the SvelteKit server
-  await initialized;
-  return server.respond(request, {
-    platform: {context},
-    getClientAddress: () => clientAddress
-  });
-};
-
-serve(handler, {
-  hostname: '0.0.0.0',
-  port: 3000
-});
+);
